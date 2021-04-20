19516eb5d86ce3c8b7d93213b716717ce7a811ec
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-20 22:20:35 +0800

    创建了项目

---
+++ covid_19_visualization/china_map/src/main/java/edu/guet/ChinaMapApplication.java
@@ -0,0 +1,18 @@
+package edu.guet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XUAN
+ * @date 2021/4/20 - 22:17
+ * @references
+ * @purpose
+ * @errors
+ */
+@SpringBootApplication
+public class ChinaMapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChinaMapApplication.class, args);
+    }
+}
