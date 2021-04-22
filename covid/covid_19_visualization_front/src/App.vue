ed4d640ee7683f20a65b61c205e0c10fe2dfa7d2
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-22 21:19:03 +0800

    把 HelloWorld.vue 放到 Index.vue 页面中

--- covid/covid_19_visualization_front/src/App.vue
+++ covid/covid_19_visualization_front/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index></Index>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Index from '@/pages/Index'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
---
+++ covid/covid_19_visualization_front/src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ChinaMap></ChinaMap>
+  </div>
+</template>
+
+<script>
+import ChinaMap from '@/components/ChinaMap'
+export default {
+  components: {
+    ChinaMap
+  }
+};
+</script>
+<style>
+</style>
+
