a024ec6fa67fa92a5d407d9c3f8c4d38b6c157ec
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-23 11:39:32 +0800

    格式化文档

--- covid/covid_19_visualization_front/src/components/DetailVisualMapHorizontal.vue
+++ covid/covid_19_visualization_front/src/components/DetailVisualMapHorizontal.vue
@@ -10,63 +10,61 @@ export default {
     mapData: {},
   },
   data() {
-    return {
-    };
+    return {};
   },
   mounted() {
-    
     if (this.mapData.length > 0) {
-        this.myEcharts();
+      this.myEcharts();
     }
   },
 
   methods: {
-
     myEcharts() {
       // 基于准备好的dom，初始化echarts实例
-      var myChart = this.$echarts.init(document.getElementById("detail_visualMap_horizontal"));
-              let data = []
-        data.push('value','name')
-        for(let i=0;i<this.mapData.length;i++){
-            let temp = []
-            temp.push(this.mapData[i].value)
-            temp.push(this.mapData[i].name)
-            data.push(temp)
-        }
-        console.log(data)
+      var myChart = this.$echarts.init(
+        document.getElementById("detail_visualMap_horizontal")
+      );
+      let data = [];
+      data.push("value", "name");
+      for (let i = 0; i < this.mapData.length; i++) {
+        let temp = [];
+        temp.push(this.mapData[i].value);
+        temp.push(this.mapData[i].name);
+        data.push(temp);
+      }
+      console.log(data);
 
-
-              let options = {
-                dataset: {
-                    source: data
-                },
-                grid: {containLabel: true},
-                xAxis: {name: 'value'},
-                yAxis: {type: 'category'},
-                visualMap: {
-                    orient: 'horizontal',
-                    left: 'center',
-                    min: 0,
-                    max: 100,
-                    text: ['High value', 'Low value'],
-                    // Map the value column to color
-                    dimension: 0,
-                    inRange: {
-                        color: ['#D7DA8B', '#E15457']
-                    }
-                },
-                series: [
-                    {
-                        type: 'bar',
-                        encode: {
-                            // Map the "amount" column to X axis.
-                            x: 'value',
-                            // Map the "name" column to Y axis
-                            y: 'name'
-                        }
-                    }
-                ]
-            };
+      let options = {
+        dataset: {
+          source: data,
+        },
+        grid: { containLabel: true },
+        xAxis: { name: "value" },
+        yAxis: { type: "category" },
+        visualMap: {
+          orient: "horizontal",
+          left: "center",
+          min: 0,
+          max: 100,
+          text: ["High value", "Low value"],
+          // Map the value column to color
+          dimension: 0,
+          inRange: {
+            color: ["#D7DA8B", "#E15457"],
+          },
+        },
+        series: [
+          {
+            type: "bar",
+            encode: {
+              // Map the "amount" column to X axis.
+              x: "value",
+              // Map the "name" column to Y axis
+              y: "name",
+            },
+          },
+        ],
+      };
 
       myChart.setOption(options);
     },
