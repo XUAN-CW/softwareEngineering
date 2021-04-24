23dd805e0da628d5e11b74bc8e359bfad1e802e2
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-24 16:33:41 +0800

    修改 Pie4.vue 使其更加符合本项目

--- covid/covid_19_visualization_front/src/components/Pie4.vue
+++ covid/covid_19_visualization_front/src/components/Pie4.vue
@@ -21,18 +21,11 @@ export default {
   methods: {
     myEcharts() {
       // 基于准备好的dom，初始化echarts实例
-      var myChart = this.$echarts.init(
-        document.getElementById("pie4")
-      );
-      let data = [];
-      data.push("value", "name");
+      var myChart = this.$echarts.init(document.getElementById("pie4"));
+      let legendData = [];
       for (let i = 0; i < this.mapData.length; i++) {
-        let temp = [];
-        temp.push(this.mapData[i].value);
-        temp.push(this.mapData[i].name);
-        data.push(temp);
+        legendData.push(this.mapData[i].name);
       }
-      // console.log(data);
 
       let option = {
         title: {
@@ -48,15 +41,7 @@ export default {
           orient: "horizontal", // 标签名称垂直排列
           x: "center", // 标签的位置
           y: "bottom",
-          data: [
-            "不能坚持健身",
-            "偏好其他健身场所",
-            "健身房价格太贵",
-            "部分健身房有强行推销的行为",
-            "健身房空气不流通",
-            "健身房人多繁杂",
-            "对市面上的健身房都不满意",
-          ],
+          data: legendData,
         }, // 标签变量名称
         toolbox: {
           show: true,
@@ -74,7 +59,7 @@ export default {
         calculable: true,
         series: [
           {
-            name: "面积模式", // 图表名称
+            name: "疫情", // 图表名称
             type: "pie", // 图表类型
             radius: [30, 160], // 图表内外半径大小
             center: ["50%", "50%"], // 图表位置
@@ -85,15 +70,7 @@ export default {
                 formatter: "{b}({d}%)", // 显示百分比
               },
             },
-            data: [
-              { value: 180, name: "不能坚持健身" }, // 变量对应的具体数据
-              { value: 180, name: "偏好其他健身场所" },
-              { value: 165, name: "健身房价格太贵" },
-              { value: 151, name: "部分健身房有强行推销的行为" },
-              { value: 97, name: "健身房空气不流通" },
-              { value: 81, name: "健身房人多繁杂" },
-              { value: 29, name: "对市面上的健身房都不满意" },
-            ],
+            data: this.mapData,
           },
         ],
       };
@@ -107,7 +84,7 @@ export default {
 #pie4 {
   margin: 20px auto; /* 外边距 */
   width: 1000px;
-  height: 520px;
+  height: 620px;
 }
 </style>
 
