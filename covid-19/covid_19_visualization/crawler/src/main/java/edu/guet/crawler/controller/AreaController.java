eff833884fbd6085dce5a2eb4885b9f68c6b6c3c
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-05-02 11:47:45 +0800

    可通过 controller 访问

--- covid-19/covid_19_visualization/crawler/src/main/java/edu/guet/crawler/controller/AreaController.java
+++ covid-19/covid_19_visualization/crawler/src/main/java/edu/guet/crawler/controller/AreaController.java
@@ -1,6 +1,11 @@
 package edu.guet.crawler.controller;
 
 
+import edu.guet.crawler.entity.vo.AreaWithChildren;
+import edu.guet.crawler.qq_crawler.CovidInChina;
+import edu.guet.crawler.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/crawler/area")
 public class AreaController {
+
+    @Autowired
+    CovidInChina covidInChina;
+
+    //http://localhost:8801//crawler/area/getChina
+    @GetMapping("getChina")
+    public AreaWithChildren getChina(){
+        return covidInChina.currentChinaData();
+    }
 
 }
 
